e9b571275f1a87fd9e70fcdc041bfbc36c87bae0
Ranadeep Polavarapu <ranadeep@icloud.com>
2016-05-13 22:01:48 -0400

    add airbnb ESLint file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var polyjuice = require('polyjuice')
 
 var to_eslint = {
-  jshint: polyjuice.from.jshint('./.jshintrc'),
   jscs  : polyjuice.from.jscs('./.jscsrc'),
-  both  : polyjuice.to.eslint('./.jshintrc', './.jscsrc')
 }
